3478cef9c099ff66e38b91217dcbe8a52ba51c3d
SophieRG1 <123910034+SophieRG1@users.noreply.github.com>
2023-11-22 13:44:25 -0600

    Update microbit_uart_ble.js

--- js_ble/microbit_uart_ble.js
+++ js_ble/microbit_uart_ble.js
@@ -1,4 +1,4 @@
-const UART_SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+const UART_SERVICE_UUID =           "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
 const UART_TX_CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
 const UART_RX_CHARACTERISTIC_UUID = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 
@@ -35,7 +35,7 @@ async function connectButtonPressed() {
     rxCharacteristic = await service.getCharacteristic(
       UART_RX_CHARACTERISTIC_UUID
     );
-    document.getElementById('robotShow').classList.add("robotShow_connected");
+    document.getElementById('bluetooth').classList.add("bluetooth_connected");
   } catch (error) {
     console.log(error);
   }
@@ -60,7 +60,7 @@ async function sendUART(num) {
   let encoder = new TextEncoder();
   queueGattOperation(() => rxCharacteristic.writeValue(encoder.encode(num+"\n"))
       .then(() => console.log("WRITE"))
-      .catch(error => console.error('Błąd podczas wysyłania danych:', error)));
+      .catch(error => console.error('Error', error)));
 }
 
 
@@ -88,10 +88,8 @@ function onTxCharacteristicValueChanged(event) {
   }
 }
 
-
-
 function onDisconnected(event) {
   let device = event.target;
   console.log(`Device ${device.name} is disconnected.`);
-  document.getElementById('robotShow').classList.remove("robotShow_connected");
+  document.getElementById('bluetooth').classList.remove("bluetooth_connected");
 }
